b4d02d3a73dad82ba02f5bb30d8842d38e34fb8c
adony-lagares <adonyhibari48@gmail.com>
2022-02-01 23:41:28 -0300

    calculadora java script

---
+++ assets/calculadora.js
@@ -0,0 +1,72 @@
+function calculadora() {
+    const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - Divisão Real (/)\n 5 - Divisão Inteira (%))\n 6 - Potenciação (**)'));
+
+    if (!operacao ||  operacao >= 7) {
+        alert('erro - operação inválida');
+        calculadora();}
+ 
+    let n1 = Number(prompt('Insira o primeiro valor:'));
+    let n2 = Number(prompt('Insira o segundo valor:'));
+    let resultado;
+
+    if (!n1 || !n2) {
+        alert('erro - parâmetros inválidos');
+        calculadora();}
+
+ function soma() {
+     resultado = n1 + n2;
+     alert(`${n1} + ${n2} = ${resultado}`);
+     novaOperacao();
+ }
+ function subtracao() {
+    resultado = n1 - n2;
+    alert(`${n1} - ${n2} = ${resultado}`);
+    novaOperacao();
+}
+function multiplicacao() {
+    resultado = n1 * n2;
+    alert(`${n1} * ${n2} = ${resultado}`);
+    novaOperacao();
+}
+function divisaoReal() {
+    resultado = n1 / n2;
+    alert(`${n1} / ${n2} = ${resultado}`);
+    novaOperacao();
+}
+function divisaoInteira() {
+    resultado = n1 % n2;
+    alert(`${n1} % ${n2} = ${resultado}`);
+    novaOperacao();
+}
+function potenciacao() {
+    resultado = n1 ** n2;
+    alert(`${n1} ** ${n2} = ${resultado}`);
+    novaOperacao();
+}
+
+function novaOperacao() {
+    let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+    if (opcao == 1) {
+        calculadora();
+    } else if (opcao == 2) {
+        alert('Até mais!');
+    } else {
+        alert('Digite uma opção válida!');
+        novaOperacao();
+    }
+}
+ if (operacao == 1) {
+     soma();
+ } else if (operacao ==2){
+     subtracao();
+ } else if (operacao ==3){
+    multiplicacao();
+}  else if (operacao ==4){
+    divisaoReal();
+}  else if (operacao ==5){
+    divisaoInteira();
+}  else if (operacao ==6){
+    potenciacao();
+}
+}
+calculadora();
